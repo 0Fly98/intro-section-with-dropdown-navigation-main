a2c5c168a403d134a2903dee6dc0749c24a86dc9
joshua lundu <122810726+0Fly98@users.noreply.github.com>
2023-05-08 16:29:38 +0200

    web-design

---
+++ index.js
@@ -0,0 +1,8 @@
+const featureBtn = document.getElementById('feature-btn');
+const companyBtn = document.getElementById("company-btn");
+const featureBlog = document.getElementById('feature-blog');
+const companyBlog = document.getElementById('company-blog')
+
+featureBtn.addEventListener('click',() => {
+    featureBlog.classList.remove = 'display-none'
+})
